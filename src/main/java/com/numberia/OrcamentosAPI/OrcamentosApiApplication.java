a9150d2e211c027f61c23b18964ac2867b982c92
nouqens <gmpunisher490@gmail.com>
2025-07-02 11:16:07 -0300

    first commit

---
+++ src/main/java/com/numberia/OrcamentosAPI/OrcamentosApiApplication.java
@@ -0,0 +1,13 @@
+package com.numberia.OrcamentosAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentosApiApplication.class, args);
+	}
+
+}
