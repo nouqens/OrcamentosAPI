715d59b23030e732ac9e1d31c2cf5176e2de0d21
nouqens <gmpunisher490@gmail.com>
2025-07-04 14:38:22 -0300

    feat(repository): cria o Repository da entidade Usuario

--- src/main/java/com/numberia/OrcamentosAPI/Repository/UsuarioRepository.java
+++ src/main/java/com/numberia/OrcamentosAPI/Repository/UsuarioRepository.java
@@ -1,4 +1,9 @@
 package com.numberia.OrcamentosAPI.Repository;
 
-public class UsuarioRepository {
+import com.numberia.OrcamentosAPI.Models.UsuarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, UUID> {
 }
