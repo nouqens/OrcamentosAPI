e2cd186b2742acb5517a21c733826a7a9a09dfda
nouqens <gmpunisher490@gmail.com>
2025-07-04 14:39:16 -0300

    feat(service): cria o Service da entidade Usuario

--- src/main/java/com/numberia/OrcamentosAPI/Services/UsuarioService.java
+++ src/main/java/com/numberia/OrcamentosAPI/Services/UsuarioService.java
@@ -1,4 +1,34 @@
 package com.numberia.OrcamentosAPI.Services;
 
-public class UsuarioServices {
+import com.numberia.OrcamentosAPI.Models.UsuarioModel;
+import com.numberia.OrcamentosAPI.Repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public List<UsuarioModel> getAll(){
+        return usuarioRepository.findAll();
+    }
+
+    public UsuarioModel save(UsuarioModel usuarioModel){
+        return usuarioRepository.save(usuarioModel);
+    }
+    public void delete(UUID id){
+        usuarioRepository.deleteById(id);
+
+
+    }
+
+
 }
