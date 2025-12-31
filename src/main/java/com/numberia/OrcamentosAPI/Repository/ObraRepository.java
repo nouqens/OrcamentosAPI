f1446419a58a6142dc31966989044938fbbd3e29
nouqens <gmpunisher490@gmail.com>
2025-12-31 13:28:55 -0300

    feat(obra): adiciona query para obtenção das obras do usuário

--- src/main/java/com/numberia/OrcamentosAPI/Repository/ObraRepository.java
+++ src/main/java/com/numberia/OrcamentosAPI/Repository/ObraRepository.java
@@ -1,9 +1,29 @@
 package com.numberia.OrcamentosAPI.Repository;
 
+import com.numberia.OrcamentosAPI.DTOs.ObraDTO;
 import com.numberia.OrcamentosAPI.Models.ObraModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ObraRepository extends JpaRepository<ObraModel, UUID> {
+    @Query("""
+       SELECT new com.numberia.OrcamentosAPI.DTOs.ObraDTO(
+           o.id,
+           o.titulo,
+           o.status,
+           o.descricao,
+           u.id,
+           c.id
+           
+       )
+       FROM ObraModel o
+       JOIN o.usuario u
+       LEFT JOIN o.cliente c
+       WHERE u.id = :usuarioId
+       """)
+    List<ObraDTO> findByUsuarioId(UUID usuarioId);
+
 }
