0a696fc1b902eb20f93ebc5bb8fc7559b04b1c42
nouqens <gmpunisher490@gmail.com>
2025-12-31 13:45:54 -0300

    fix(cliente): corrige queries para JPQL

--- src/main/java/com/numberia/OrcamentosAPI/Repository/ClienteRepository.java
+++ src/main/java/com/numberia/OrcamentosAPI/Repository/ClienteRepository.java
@@ -1,20 +1,24 @@
 package com.numberia.OrcamentosAPI.Repository;
 
+
 import com.numberia.OrcamentosAPI.Models.ClienteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<ClienteModel, UUID> {
 
-    @Query(value = "SELECT c FROM tb_clientes c WHERE c.nome = :name", nativeQuery = true)
-    List<ClienteModel> findByName(@Param("name") String name);
+    @Query("SELECT c FROM ClienteModel c WHERE c.nome = :nome")
+    Optional<ClienteModel> findByNome(@Param("nome") String nome);
+
+    @Query("SELECT c FROM ClienteModel c WHERE c.usuario.id = :usuarioId")
+    List<ClienteModel> findByUsuarioId(@Param("usuarioId") UUID usuarioId);
 
 
-    @Query(value = "SELECT * FROM tb_clientes WHERE usuario_id = :usuario_id", nativeQuery = true)
-    List<ClienteModel> findClienteByUsuarioId(@Param("usuario_id") UUID id);
 }
