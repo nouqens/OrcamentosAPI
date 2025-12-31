f78dc8c5d42d1027e71c01c6659dc4f6469289aa
nouqens <gmpunisher490@gmail.com>
2025-12-31 13:21:20 -0300

    feat(recurso): adiciona query de busca de recursos do usuário

--- src/main/java/com/numberia/OrcamentosAPI/Repository/RecursoRepository.java
+++ src/main/java/com/numberia/OrcamentosAPI/Repository/RecursoRepository.java
@@ -1,9 +1,27 @@
 package com.numberia.OrcamentosAPI.Repository;
 
+import com.numberia.OrcamentosAPI.DTOs.RecursoDTO;
 import com.numberia.OrcamentosAPI.Models.RecursoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RecursoRepository extends JpaRepository<RecursoModel, UUID> {
+    @Query("""
+    SELECT new com.numberia.OrcamentosAPI.DTOs.RecursoDTO(
+        r.id,
+        r.descricao,
+        r.unidade,
+        r.precoUnitario,
+        r.tipo,
+        u.id
+    )
+    FROM RecursoModel r
+    JOIN r.usuario u
+    WHERE u.id = :usuarioId
+    """)
+    List<RecursoDTO> findAllByUser(@Param("usuarioId") UUID usuarioId);
 }
