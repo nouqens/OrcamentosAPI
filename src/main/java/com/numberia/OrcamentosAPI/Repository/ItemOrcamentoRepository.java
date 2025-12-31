60ba3a78dfc20fe3c2cc9c49159ba6724486eadd
nouqens <gmpunisher490@gmail.com>
2025-12-31 13:35:17 -0300

    feat(itemorcamento): adiciona query para obtenção de itens da obra

--- src/main/java/com/numberia/OrcamentosAPI/Repository/ItemOrcamentoRepository.java
+++ src/main/java/com/numberia/OrcamentosAPI/Repository/ItemOrcamentoRepository.java
@@ -3,7 +3,9 @@ package com.numberia.OrcamentosAPI.Repository;
 import com.numberia.OrcamentosAPI.Models.ItemOrcamentoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ItemOrcamentoRepository extends JpaRepository<ItemOrcamentoModel, UUID> {
+    List<ItemOrcamentoModel> findByObra_Id(UUID obraId);
 }
