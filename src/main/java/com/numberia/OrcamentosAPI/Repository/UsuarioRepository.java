1d6a744a55b9210d3953c0e460309146509833d6
nouqens <gmpunisher490@gmail.com>
2025-12-31 13:05:08 -0300

    feat(usuario): adiciona busca de usuário por e-mail

--- src/main/java/com/numberia/OrcamentosAPI/Repository/UsuarioRepository.java
+++ src/main/java/com/numberia/OrcamentosAPI/Repository/UsuarioRepository.java
@@ -4,9 +4,11 @@ import com.numberia.OrcamentosAPI.Models.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, UUID> {
 
     UserDetails findByEmail(String email);
+    Optional<UsuarioModel> findOptionalByEmail(String email);
 }
