2c80672b3f900175616d0efeced292fedd7f6eeb
nouqens <gmpunisher490@gmail.com>
2025-12-31 13:19:38 -0300

    feat(recurso): adiciona construtor com dependência de model

--- src/main/java/com/numberia/OrcamentosAPI/Models/RecursoModel.java
+++ src/main/java/com/numberia/OrcamentosAPI/Models/RecursoModel.java
@@ -1,13 +1,21 @@
 package com.numberia.OrcamentosAPI.Models;
 
 
-import jakarta.persistence.*;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.UUID;
 
 @Setter
 @Getter
@@ -30,6 +38,19 @@ public class RecursoModel {
     @JoinColumn(name = "usuario_id", nullable = false)
 
     private UsuarioModel usuario;
+
+
+    public RecursoModel() {
+    }
+
+    public RecursoModel(UUID id, String descricao, String unidade, double precoUnitario, String tipo, UsuarioModel usuario) {
+        this.id = id;
+        this.descricao = descricao;
+        this.unidade = unidade;
+        this.precoUnitario = precoUnitario;
+        this.tipo = tipo;
+        this.usuario = usuario;
+    }
 
     public UUID getId() {
         return id;
